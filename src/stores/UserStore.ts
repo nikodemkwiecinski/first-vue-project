63b253d3b9283fe62bb8b70c6d092507eba63b3d
nikodemkwiecinski <nikokwiatek@gmail.com>
2022-06-19 14:03:14 +0200

    Early setup for Pinia storage

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -2,18 +2,31 @@ import { defineStore } from 'pinia'
 import { UserStateProps } from '../types/UserType'
 
 export const useUserStore = defineStore('UserStore', {
-  state: (): UserStateProps => ({
-    users: [],
-  }),
+ state: (): UserStateProps => ({
+  users: [],
+  activeUser: null,
+ }),
 
-  actions: {
-    addUser(login: string, password: string) {
-      const newUsers = [...this.users]
-      newUsers.push({ id: new Date().getTime(), login, password })
-      this.users = [...newUsers]
-    },
-    delUser(userId: number) {
-      this.users = this.users.filter((user) => user.id !== userId)
-    },
+ actions: {
+  addUser(login: string, password: string) {
+   const newUsers = [...this.users]
+   newUsers.push({
+    id: new Date().getTime(),
+    login,
+    password,
+    favouriteFoods: [],
+   })
+   this.users = [...newUsers]
   },
+  delUser(userId: number) {
+   this.users = this.users.filter((user) => user.id !== userId)
+  },
+
+  validateUser(login: string, password: string) {
+   const testPassword = this.users.find(
+    (user) => user.login === login && user.password === password
+   )
+   this.activeUser = testPassword !== undefined ? testPassword.id : null
+  },
+ },
 })
--- src/types/UserType.ts
+++ src/types/UserType.ts
@@ -1,9 +1,11 @@
 export interface UserStateProps {
-  users: Array<UserProps>
+ users: Array<UserProps>
+ activeUser: null | number
 }
 
 export interface UserProps {
-  id: number
-  login: string
-  password: string
+ id: number
+ login: string
+ password: string
+ favouriteFoods: Array<string>
 }
